2fc0667addcd994a11b2e66044edf4d69c5fbeb7
KolyshkinEfim <kolyshkin.efim@mail.ru>
2023-03-08 19:01:03 +0300

    header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/index.css";
+import Header from "./components/header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
